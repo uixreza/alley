36de88a7776e426d28caae3e38e90f3daa31d601
Reza Kamali <mrgando@protonmail.com>
2025-06-25 12:19:24 +0330

    small changes

--- src/components/UI/Socials.tsx
+++ src/components/UI/Socials.tsx
@@ -5,8 +5,8 @@ import { FaInstagram } from "react-icons/fa6";
 
 export default function Socials() {
   return (
-    <div className="flex flex-row-reverse justify-between items-center w-full container">
-      <span className="shadow-2xl shadow-black">
+    <div className="flex flex-row-reverse sm:justify-between justify-center items-center w-full container">
+      <span className="shadow-2xl shadow-black hidden sm:block">
         توسعه با 💪 توسط تیم هومینکس
       </span>
       <div className="flex flex-row gap-5 text-2xl py-5">
